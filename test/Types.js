7e4c04472ff3cac38b667339f88c1648868c2e5a
David Pineda <david.pineda.orvc@gmail.com>
2023-03-01 16:42:34 -0500

    start Types review

---
+++ test/Types.js
@@ -0,0 +1,19 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('CONTRACT_DESC_HERE', () => {
+  beforeEach(async () => {
+    const ContractFactory = await ethers.getContractFactory('SOL_FILE_HERE');
+    contract = await Contract.deploy();
+  });
+
+  describe('FEATURE_DESC', () => {
+    beforeEach(async () => {
+
+    });
+
+    it('EXPECTATION_DESC', async () => {
+
+    });
+  });
+});
